c4db45bb78750c9fe7d890fd9083ae16a7b458ef
Tong <tong.dustin@gmail.com>
2020-06-24 22:37:37 -0700

    Initial Commit

---
+++ 11health Code Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  11health Code Challenge
+//
+//  Created by dustin on 24/6/2020.
+//  Copyright © 2020 Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 11health Code ChallengeTests/_1health_Code_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  _1health_Code_ChallengeTests.swift
+//  11health Code ChallengeTests
+//
+//  Created by dustin on 24/6/2020.
+//  Copyright © 2020 Jan. All rights reserved.
+//
+
+import XCTest
+@testable import _1health_Code_Challenge
+
+class _1health_Code_ChallengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
